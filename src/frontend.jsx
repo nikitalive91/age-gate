8525387a40d60b7b14b1b09f99cb905a5e23502b
Nikita <karpovnik91@gmail.com>
2026-03-25 23:14:11 +0200

    Fix IDKit: export useIDKit, use setOpen(true) via useEffect to open modal

--- src/frontend.jsx
+++ src/frontend.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { IDKitWidget } from '@worldcoin/idkit'
+import ReactDOM from 'react-dom/client'
+import { IDKitWidget, useIDKit } from '@worldcoin/idkit'
 
 // Expose to global scope for use in index.html
 window.React = React
 window.ReactDOM = ReactDOM
-window.IDKit = { IDKitWidget }
+window.IDKit = { IDKitWidget, useIDKit }
